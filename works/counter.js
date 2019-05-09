2653c8876fe61106157569fe8a86ac136f3221b7
ramune <wewewewest12@gmail.com>
2019-05-09 19:46:04 +0900

    むっ

--- works/counter.js
+++ works/counter.js
@@ -7,5 +7,5 @@ const countUp = () => {
 const countReset = () => {
     count = 0
     const countElement = document.querySelector('#count')
-  countElement.innerText = '回数: ' + count
+    countElement.innerText = '回数: ' + count
 }
